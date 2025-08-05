28ef7bb02dbe1b8974e1a25e5a58006060c8fe90
Thees K <ts.custer@t-online.de>
2025-08-05 14:10:31 +0200

    chore(resource): remove unused imports from RestConfiguration.java

--- src/main/java/k/thees/resource/RestConfiguration.java
+++ src/main/java/k/thees/resource/RestConfiguration.java
@@ -1,10 +1,7 @@
 package k.thees.resource;
 
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
 /**
- * JAX-RS application configuration, configures RESTful Web Services for the application
+ * JAX-RS application configuration, configures RESTfl Web Services for the application
  */
 // @ApplicationPath("api")
 public class RestConfiguration /* extends Application */ {
