cb9fa84dba78c8a7127106bc7966da4eca55843a
Thees K <ts.custer@t-online.de>
2025-08-11 17:10:42 +0200

    chore(config): remove unused RestConfiguration to avoid JAX-RS config conflicts

--- src/main/java/k/thees/resource/RestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package k.thees.resource;
-
-/**
- * JAX-RS application configuration, configures RESTfl Web Services for the application
- */
-@ApplicationPath("api")
-public class RestConfiguration /* extends Application */ {
-    //
-}
