c0d04339acfa1444713820dead75e12fab1cab7e
Thees K <ts.custer@t-online.de>
2025-08-26 11:32:44 +0200

    style: remove commented unused method from UsernameAlreadyExistsException

--- src/main/java/k/thees/security/UsernameAlreadyExistsException.java
+++ src/main/java/k/thees/security/UsernameAlreadyExistsException.java
@@ -4,8 +4,4 @@ public class UsernameAlreadyExistsException extends RuntimeException {
     public UsernameAlreadyExistsException(String username) {
         super(String.format("User with name %s already exists. Please choose a different username.", username));
     }
-
-//    public static void throwForUsername(String username) {
-//        throw new UsernameAlreadyExistsException(username);
-//    }
 }
