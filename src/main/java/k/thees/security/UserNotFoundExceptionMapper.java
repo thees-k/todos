e58ffc2256353e982db11ed8d2a9755617c7b2d1
Thees K <ts.custer@t-online.de>
2025-08-26 13:48:26 +0200

    feat(security): implement UserNotFoundExceptionMapper to map exception to 404 response

---
+++ src/main/java/k/thees/security/UserNotFoundExceptionMapper.java
@@ -0,0 +1,14 @@
+package k.thees.security;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class UserNotFoundExceptionMapper implements ExceptionMapper<UserNotFoundException> {
+
+    @Override
+    public Response toResponse(UserNotFoundException exception) {
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+}
